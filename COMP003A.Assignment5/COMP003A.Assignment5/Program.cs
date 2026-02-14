3a47ba7ccf61d6d6579bd0588369b5184ce02b78
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-13 16:53:10 -0800

    created step 1 with the program introduction and step 2 user prompt for name and number

---
+++ COMP003A.Assignment5/COMP003A.Assignment5/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace COMP003A.Assignment5
+{
+    internal class Program
+    {
+        static void DisplayIntroduction()
+        { Console.WriteLine("Welcome to the Program Organizer!\n"); }
+        
+        
+        static void Main(string[] args)
+        {
+            DisplayIntroduction();
+
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.Write("Enter a number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+        }
+    }
+}
