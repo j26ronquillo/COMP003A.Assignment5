7e498e29d2eeedd4e33968a1746a4e19343c4ad9
j26ronquillo <joshalvinpogi26@yahoo.com>
2026-02-13 17:07:40 -0800

    created the method for the calculated need in step 3 and displayed it in main.

--- COMP003A.Assignment5/COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/COMP003A.Assignment5/Program.cs
@@ -4,16 +4,23 @@
     {
         static void DisplayIntroduction()
         { Console.WriteLine("Welcome to the Program Organizer!\n"); }
-        
+
+        static int CalculatedValue(int doubledNumber)
+        { int finalAnswer = doubledNumber * 2;
+        return finalAnswer;}
         
         static void Main(string[] args)
         {
             DisplayIntroduction();
 
+
             Console.Write("Enter your name: ");
             string userName = Console.ReadLine();
             Console.Write("Enter a number: ");
             int userNumber = int.Parse(Console.ReadLine());
+
+            int finalAnswer = CalculatedValue(userNumber);
+            Console.WriteLine($"\nCalculated Result: {finalAnswer}");
         }
     }
 }
